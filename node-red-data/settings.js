c2da3f5ed5439cc6541da21ac963149ca560101b
Leonid Kalintsev <lensuperhero@gmail.com>
2026-06-30 15:29:58 +0300

    test3

--- node-red-data/settings.js
+++ node-red-data/settings.js
@@ -41,7 +41,7 @@ module.exports = {
      * node-red from being able to decrypt your existing credentials and they will be
      * lost.
      */
-    //credentialSecret: "a-secret-key",
+    credentialSecret: "aKdwIJ5B60X1Vh0q5xNkLAFp",
 
     /** By default, the flow JSON will be formatted over multiple lines making
      * it easier to compare changes when using version control.
